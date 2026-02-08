77da4ad06743dc1685292a8486f3fb60d9db2710
RayOfSteel <rainer.stahl42@outlook.com>
2026-02-08 11:32:48 +0100

    feat(01-01): install Tailwind toolchain and config
    - add Tailwind, PostCSS, and Autoprefixer dev deps
    - add tailwind and postcss configuration files
    - add Tailwind entry stylesheet

---
+++ site/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}", "./templates/**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
